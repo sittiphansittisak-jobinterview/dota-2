f94e57dda658eea9a2db878c220f68066b0b2c43
สิทธิพันธ์ สิทธิศักดิ์ <sittiphansittisak.jobinterview@gmail.com>
2023-11-02 16:04:35 +0700

    update heroDatabase for all hero

--- dota_2_client_flutter/lib/database/hero_database.dart
+++ dota_2_client_flutter/lib/database/hero_database.dart
@@ -1,13 +1,15 @@
+import 'package:dota_2_client_flutter/config/hero_name.dart';
+import 'package:dota_2_client_flutter/config/video_asset_path.dart';
 import 'package:dota_2_client_flutter/model/hero_thumbnail_model.dart';
 
 class HeroDatabase {
   static Future<List<HeroThumbnailModel>?> getThumbnailList() async {
-    final data = [
-      {
-        'name': 'Abaddon',
-        'video': 'asset/video/hero/npc_dota_hero_abaddon.webm',
-      },
-    ];
+    final data = HeroName.list
+        .map<Map<String, dynamic>>((e) => {
+              'name': e,
+              'video': VideoAssetPath.heroPage.getPathByHeroName(e),
+            })
+        .toList();
     return data.map((e) => HeroThumbnailModel.fromJson(e)).toList();
   }
 }
